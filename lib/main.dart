dafa963bfabf2a8c3a9cd82b1cb1867bc149ec7b
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2019-07-08 10:24:27 -0300

    Chamando API

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+const request = "https://api.hgbrasil.com/finance?key=fead6306";
+
+void main() async{
+  print (await getData());
+  runApp(MaterialApp(
+    home: Container()
+  ));
+}
+
+Future<Map> getData() async{
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
